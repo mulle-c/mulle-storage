baf873037cea0b4180c6349043ed4e68a9a2a4e7
Nat! <nat@mulle-kybernetik.com>
2024-01-11 23:19:18 +0100

    updated project

--- cmake/reflect/_Dependencies.cmake
+++ cmake/reflect/_Dependencies.cmake
@@ -17,64 +17,64 @@ endif()
 # Disable for this platform: `mulle-sourcetree mark mulle-container no-cmake-platform-${MULLE_UNAME}`
 # Disable for a sdk: `mulle-sourcetree mark mulle-container no-cmake-sdk-<name>`
 #
-if( NOT MULLE_CONTAINER_LIBRARY)
-   find_library( MULLE_CONTAINER_LIBRARY NAMES
+if( NOT MULLE__CONTAINER_LIBRARY)
+   find_library( MULLE__CONTAINER_LIBRARY NAMES
       ${CMAKE_STATIC_LIBRARY_PREFIX}mulle-container${CMAKE_DEBUG_POSTFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}
       ${CMAKE_STATIC_LIBRARY_PREFIX}mulle-container${CMAKE_STATIC_LIBRARY_SUFFIX}
       mulle-container
       NO_CMAKE_SYSTEM_PATH NO_SYSTEM_ENVIRONMENT_PATH
    )
-   if( NOT MULLE_CONTAINER_LIBRARY AND NOT DEPENDENCY_IGNORE_SYSTEM_LIBARIES)
-      find_library( MULLE_CONTAINER_LIBRARY NAMES
+   if( NOT MULLE__CONTAINER_LIBRARY AND NOT DEPENDENCY_IGNORE_SYSTEM_LIBARIES)
+      find_library( MULLE__CONTAINER_LIBRARY NAMES
          ${CMAKE_STATIC_LIBRARY_PREFIX}mulle-container${CMAKE_DEBUG_POSTFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}
          ${CMAKE_STATIC_LIBRARY_PREFIX}mulle-container${CMAKE_STATIC_LIBRARY_SUFFIX}
          mulle-container
       )
    endif()
-   message( STATUS "MULLE_CONTAINER_LIBRARY is ${MULLE_CONTAINER_LIBRARY}")
+   message( STATUS "MULLE__CONTAINER_LIBRARY is ${MULLE__CONTAINER_LIBRARY}")
    #
    # The order looks ascending, but due to the way this file is read
    # it ends up being descending, which is what we need.
    #
-   if( MULLE_CONTAINER_LIBRARY)
+   if( MULLE__CONTAINER_LIBRARY)
       #
-      # Add MULLE_CONTAINER_LIBRARY to DEPENDENCY_LIBRARIES list.
+      # Add MULLE__CONTAINER_LIBRARY to DEPENDENCY_LIBRARIES list.
       # Disable with: `mulle-sourcetree mark mulle-container no-cmake-add`
       #
-      list( APPEND DEPENDENCY_LIBRARIES ${MULLE_CONTAINER_LIBRARY})
+      list( APPEND DEPENDENCY_LIBRARIES ${MULLE__CONTAINER_LIBRARY})
       #
       # Inherit information from dependency.
       # Encompasses: no-cmake-searchpath,no-cmake-dependency,no-cmake-loader
       # Disable with: `mulle-sourcetree mark mulle-container no-cmake-inherit`
       #
       # temporarily expand CMAKE_MODULE_PATH
-      get_filename_component( _TMP_MULLE_CONTAINER_ROOT "${MULLE_CONTAINER_LIBRARY}" DIRECTORY)
-      get_filename_component( _TMP_MULLE_CONTAINER_ROOT "${_TMP_MULLE_CONTAINER_ROOT}" DIRECTORY)
+      get_filename_component( _TMP_MULLE__CONTAINER_ROOT "${MULLE__CONTAINER_LIBRARY}" DIRECTORY)
+      get_filename_component( _TMP_MULLE__CONTAINER_ROOT "${_TMP_MULLE__CONTAINER_ROOT}" DIRECTORY)
       #
       #
       # Search for "Definitions.cmake" and "DependenciesAndLibraries.cmake" to include.
       # Disable with: `mulle-sourcetree mark mulle-container no-cmake-dependency`
       #
-      foreach( _TMP_MULLE_CONTAINER_NAME "mulle-container")
-         set( _TMP_MULLE_CONTAINER_DIR "${_TMP_MULLE_CONTAINER_ROOT}/include/${_TMP_MULLE_CONTAINER_NAME}/cmake")
+      foreach( _TMP_MULLE__CONTAINER_NAME "mulle-container")
+         set( _TMP_MULLE__CONTAINER_DIR "${_TMP_MULLE__CONTAINER_ROOT}/include/${_TMP_MULLE__CONTAINER_NAME}/cmake")
          # use explicit path to avoid "surprises"
-         if( IS_DIRECTORY "${_TMP_MULLE_CONTAINER_DIR}")
-            list( INSERT CMAKE_MODULE_PATH 0 "${_TMP_MULLE_CONTAINER_DIR}")
+         if( IS_DIRECTORY "${_TMP_MULLE__CONTAINER_DIR}")
+            list( INSERT CMAKE_MODULE_PATH 0 "${_TMP_MULLE__CONTAINER_DIR}")
             #
-            include( "${_TMP_MULLE_CONTAINER_DIR}/DependenciesAndLibraries.cmake" OPTIONAL)
+            include( "${_TMP_MULLE__CONTAINER_DIR}/DependenciesAndLibraries.cmake" OPTIONAL)
             #
-            list( REMOVE_ITEM CMAKE_MODULE_PATH "${_TMP_MULLE_CONTAINER_DIR}")
+            list( REMOVE_ITEM CMAKE_MODULE_PATH "${_TMP_MULLE__CONTAINER_DIR}")
             #
-            unset( MULLE_CONTAINER_DEFINITIONS)
-            include( "${_TMP_MULLE_CONTAINER_DIR}/Definitions.cmake" OPTIONAL)
-            list( APPEND INHERITED_DEFINITIONS ${MULLE_CONTAINER_DEFINITIONS})
+            unset( MULLE__CONTAINER_DEFINITIONS)
+            include( "${_TMP_MULLE__CONTAINER_DIR}/Definitions.cmake" OPTIONAL)
+            list( APPEND INHERITED_DEFINITIONS ${MULLE__CONTAINER_DEFINITIONS})
             break()
          else()
-            message( STATUS "${_TMP_MULLE_CONTAINER_DIR} not found")
+            message( STATUS "${_TMP_MULLE__CONTAINER_DIR} not found")
          endif()
       endforeach()
    else()
       # Disable with: `mulle-sourcetree mark mulle-container no-require-link`
-      message( FATAL_ERROR "MULLE_CONTAINER_LIBRARY was not found")
+      message( FATAL_ERROR "MULLE__CONTAINER_LIBRARY was not found")
    endif()
 endif()
